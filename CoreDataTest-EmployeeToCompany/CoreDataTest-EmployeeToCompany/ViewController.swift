842e59926b06a4d2b2395be439a4889375660350
surnan <forsuresh@yandex.com>
2018-12-14 16:40:03 -0500

    App starts programitically

---
+++ CoreDataTest-EmployeeToCompany/CoreDataTest-EmployeeToCompany/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CoreDataTest-EmployeeToCompany
+//
+//  Created by admin on 12/14/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blue
+        
+        
+    }
+
+
+}
+
