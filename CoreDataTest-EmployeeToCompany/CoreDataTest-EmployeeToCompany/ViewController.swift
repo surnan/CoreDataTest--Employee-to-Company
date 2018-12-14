4c2ce778ef461639b1a0554be784624b4216a772
surnan <forsuresh@yandex.com>
2018-12-14 18:46:01 -0500

    tableView setup

--- CoreDataTest-EmployeeToCompany/CoreDataTest-EmployeeToCompany/ViewController.swift
+++ CoreDataTest-EmployeeToCompany/CoreDataTest-EmployeeToCompany/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let tableID = "asdfpoiu"
+    
     
     func setupNavigationStyle(){
         navigationItem.title = "Companies"
@@ -17,6 +19,7 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.barTintColor = UIColor.lightRed
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus"), style: .plain, target: self, action: #selector(handleAddCompany))
     }
     
@@ -26,10 +29,35 @@ class ViewController: UIViewController {
     }
     
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableID, for: indexPath)
+        cell.backgroundColor = .teal
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .lightBlue
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
+        tableView.backgroundColor = UIColor.darkBlue
+        
+//        tableView.separatorStyle = .none  //remove lines from every row.  Even the blank rows that don't have cell data (ex: numberOfReturns = 2)
+        tableView.separatorColor = .white
+        tableView.tableFooterView = UIView()  //lets you keep the lines ONLY between your non-nil (valid) tableViewCells (ex: numberOfReturns = 2).
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: tableID)
         setupNavigationStyle()
     }
 }
